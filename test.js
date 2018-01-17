64e5114c93aa5b723fab2c0ce2515027bc786014
Gary Dutton <garydutton@garys-mbp-2.home>
2018-01-17 16:39:32 +0000

    Completed simple styling and Exercise 3 tasks.
    Corrected links.

--- test.js
+++ test.js
@@ -2,11 +2,31 @@
 (function($, window, document) {
     // The $ is now locally scoped
 
+    //Toggle the side bar
+    function toggleSideBar(){
+        var contentElem = $('#content');
+        var sideBarElem = $('#side-bar');
+
+        if(!contentElem && !sideBarElem) return;
+
+        sideBarElem.toggleClass("sidebar-not-visible sidebar-visible")
+        contentElem.toggleClass("content-no-sidebar content-with-sidebar")
+
+    }
+
+    //Bind a click event
+    function bindClickEvent(selector, func)
+    {
+        var elem = $(selector);
+        if(elem) elem.on('click',func);
+    }
   
     // Listen for the jQuery ready event on the document
     $(function() {
-        var elem = $('main');
-        if(elem) console.log('JQuery wired up');
+        
+        //Click bindings
+        bindClickEvent('#side-bar-link',toggleSideBar);
+
     });
 
 }(window.jQuery, window, document));
